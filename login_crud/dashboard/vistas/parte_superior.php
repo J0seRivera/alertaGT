1a4311b051c57427327631463638d537dea8e72d
Jose_Rivera <joseriveradl@gmail.com>
2020-05-23 04:22:14 -0600

    inicio de sesion exitoso

--- login_crud/dashboard/vistas/parte_superior.php
+++ login_crud/dashboard/vistas/parte_superior.php
@@ -43,11 +43,11 @@ if($_SESSION["s_usuario"] === null){
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../../inicio-admin.html">
         <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">AlertaGT</div>
       </a>
 
       <!-- Divider -->
